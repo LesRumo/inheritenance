5c796ae684c09aa54b118d64e63105b4b9f2491f
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-12 16:52:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritenance
+{
+    interface area1
+    {
+        void squareArea();
+    }
+    interface area2 
+    {
+        void rectangleArea();
+    }
+     class Area: area1, area2 
+    {
+        public void squareArea() 
+        {
+            Console.WriteLine("The area of a square is sides squared(s^2)");
+        }
+
+        public void rectangleArea() 
+        {
+            Console.WriteLine("The  area of a rectangle is length * breath(l*b)");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area area = new Area();
+            area.squareArea();
+            area.rectangleArea();
+
+            Console.ReadLine();
+        }
+    }
+}
